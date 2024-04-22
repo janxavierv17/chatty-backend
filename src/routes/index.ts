be8048a8ddaa5bdf9272cd94754e400bc840a6fb
Jan Xavier Virgen <54022069+janxavierv17@users.noreply.github.com>
2024-04-22 18:18:50 +1000

    feat: health check route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,13 +3,14 @@ import { AuthRoutes } from "./auth/auth.routes";
 import { CurrentUserRoutes } from "./user/user.routes";
 import { serverAdapter } from "../services/queues/base.queue";
 import { AuthMiddleware } from "../middleware/auth.middleware";
+import { HealthCheckRoutes } from "./healthcheck";
 
 const BASE_PATH = "/api/v1";
-
 export default (app: Application) => {
     const routes = () => {
         // Bull's GUI to see if there the job
         app.use("/queues", serverAdapter.getRouter());
+        app.use(BASE_PATH, HealthCheckRoutes.healthCheck());
         app.use(BASE_PATH, AuthRoutes.routes());
         app.use(BASE_PATH, AuthRoutes.signOutRoute());
 
