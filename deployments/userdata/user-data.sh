521a4f4a1f0539bb9a4785741fa79b0c353efdbf
Jan Xavier Virgen <54022069+janxavierv17@users.noreply.github.com>
2024-04-24 13:03:17 +1000

    fix: user data script

--- deployments/userdata/user-data.sh
+++ deployments/userdata/user-data.sh
@@ -2,62 +2,46 @@
 
 function isProgramInstalled {
     local return_=1
-    echo "[isProgramInstalled] - start"
     
     type $1 >/dev/null 2>&1 || { local return_=0; }
-    
-    echo "[isProgramInstalled] - end"
     echo "$return_"
 }
 
 sudo yum update -y
 
+function isProgramInstalled {
+    type "$1" &>/dev/null
+}
+
+sudo yum update -y
+
 # Check if Node.js is installed. If not, install it
-if [ $(isProgramInstalled node) == 0 ]; then
-    echo "Installing Node.js ..."
-    
-    curl -fsSL https://rpm.nodesource.com/setup_lts.x | sudo bash -
-    yum install -y nodejs
-    
-    echo "Done installing Node.js ..."
-else
-    echo "Node.js is already installed."
+if ! isProgramInstalled node; then
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
+  
+  echo "Enabling nvm ..."
+  source ~/.bashrc
+
+  nvm install 16
+  node -e "console.log('Running Node.js ' + process.version)"
 fi
 
-if [ $(isProgramInstalled git) == 0 ]; then
-    echo "Installing git ..."
-    
-    sudo yum install git -y
-    
-    echo "Done installing git."
-else
-    echo "Git is already installed."
+if ! isProgramInstalled git; then
+  sudo yum install git -y
 fi
 
-if [ $(isProgramInstalled docker) == 0 ]; then
-    echo "Installing docker ..."
-    
-    sudo amzon-linux-extras install docker -y
-    sudo systemctl start docker
-    sudo docker run --name chatapp-redis -p 6379:6379 --restart always --detach redis
-
-    echo "Done installing docker."
-else
-    echo "Docker is already installed."
+if ! isProgramInstalled docker; then
+  sudo amazon-linux-extras install docker -y
+  sudo systemctl start docker
+  sudo docker run --name chatapp-redis -p 6379:6379 --restart always --detach redis
 fi
 
-if [ $(isProgramInstalled pm2) == 0 ]; then
-    echo "Installing pm2 ..."
-    
-    npm i -g pm2
-
-    echo "Done installing pm2."
-else
-    echo "PM2 is already installed globally."
+if ! isProgramInstalled pm2; then
+  sudo npm i -g pm2
 fi
 
 cd /home/ec2-user
-git clone -b develop https://github.com/janxavierv17/chatty-backend.git
+git clone -b feature/terraform-state https://github.com/janxavierv17/chatty-backend.git
 cd chatty-backend
 npm i
 aws s3 sync s3://chatapp-env-files/develop .
